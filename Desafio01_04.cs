06dbf30e36df4d433ef4dc23fb0a0f41fbf292f9
0bscurivo <68061944+0bscurivo@users.noreply.github.com>
2020-07-14 10:04:01 -0500

    Add files via upload

---
+++ Desafio01_04.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio01_04
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Entradas para encontrar el valor del triangulo
+            Console.Write("Ingrese b°: ");
+            double bGrados = double.Parse(Console.ReadLine());
+            Console.Write("Ingrese z: ");
+            double z = double.Parse(Console.ReadLine());
+            Console.Write("Ingrese y: ");
+            double y = double.Parse(Console.ReadLine());
+
+            double bRad = bGrados * (Math.PI / 180.0);
+
+            double cRad = Math.Atan2(z, y);
+            double eRad = Math.PI - cRad;
+            double dRad = Math.PI - eRad - bRad;
+           
+            double sobre = z / Math.Tan(dRad);
+            double resultado =(sobre - y) ;
+
+            // Salida valor solicitado
+            Console.WriteLine("El valor solicitado de b es: " + bGrados);
+            Console.WriteLine("El valor solicitado de resu es: " + resultado);
+
+        }
+    }
+}
