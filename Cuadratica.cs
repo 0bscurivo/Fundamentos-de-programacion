ae16eafe69dce2a06b52f6cb61857353e9f06e28
0bscurivo <68061944+0bscurivo@users.noreply.github.com>
2020-07-15 15:01:41 -0500

    Add files via upload

---
+++ Cuadratica.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio2Cuadratica
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            Console.Write("Calculadora de ecuaciones cuadraticas ");
+            Console.Write("Ingrese a: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Ingrese b: ");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("Ingrese c: ");
+            double c = double.Parse(Console.ReadLine());
+
+            //Discriminante
+            double dis = Math.Pow(b, 2) -4*a*c;
+            Console.Write(" El Discriminante es: " + dis);
+
+
+            // me toco divinir en pedazos para que funcionara 
+            double raiz = Math.Sqrt(dis);
+            //Console.WriteLine("raiz: " + raiz);
+            double num1 = -(b) + raiz;
+            //Console.WriteLine("numerador1: " + num1);
+            double num2 = -(b) - raiz;
+            //Console.WriteLine("numerador2: " + num2);
+            double deno = 2*a;
+            //Console.WriteLine("denominador: " + deno);
+
+            //Posibles soluciones 
+            double x1 = num1 / deno;
+            double x2 = num2 / deno;     
+            
+            if (dis == 0)
+            {
+                Console.WriteLine(" La solucion exite y es unica " );
+                Console.Write(" La solucion uno es: " + x1   );
+                Console.Write(" La solucion dos es: " + x2   );
+            }
+            else if (dis > 0)
+            {
+                Console.WriteLine("Existen dos posibles soluciones" );
+                Console.Write("La solucion uno es: " + x1     );
+                Console.Write("La solucion dos es: " + x2     );
+            }
+            else if (dis < 0)
+            {
+                Console.WriteLine(" No es posible calcular la solucion!! " );
+            }
+        }
+    }
+}
---
+++ IMC.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio1IMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bienvenido a la Calculadora de IMC");
+            Console.Write("Si quiere conocer cual es su IMC ");
+            Console.Write("Ingrese su peso (Kg): ");
+            double peso = double.Parse(Console.ReadLine());
+            Console.Write("Ingrese su altura (m): ");
+            double estatura = double.Parse(Console.ReadLine());
+            //-------------------------------------------------
+            double imc = peso / Math.Pow(estatura, 2);
+
+            if (imc < 18.5)
+            {
+                Console.WriteLine("Tu IMC es " + imc);
+                Console.WriteLine("Estas Bajo de peso!!");
+            }
+            else if ( 18.5 <=imc && imc <= 24.9)
+            {
+                Console.WriteLine("Tu IMC es " + imc);
+                Console.WriteLine("Estas Normal de peso sigue si!!");
+            }
+            else if (25 <= imc && imc <= 29.9)
+            {
+                Console.WriteLine("Tu IMC es " + imc);
+                Console.WriteLine("Estas en Sobrepeso ten cuidado!!");
+            }
+            else if (imc > 30)
+            {
+                Console.WriteLine("Tu IMC es " + imc);
+                Console.WriteLine("Estas  Obeso vaya al medico!!");
+            }
+            Console.WriteLine("Feliz dia!!");
+
+        }
+    }
+}
